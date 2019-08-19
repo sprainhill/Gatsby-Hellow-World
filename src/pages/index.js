ee24d3e7ca905aa4f7ee48b0d562c7bea73d8984
unknown <rory.christian.murray@gmail.com>
2019-08-19 11:49:48 -0400

    Hellow Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `purple` }}>
+        <h1>Hello Gatsby!</h1>
+        <div>Hello, world!</div>
+        <p>I present to you, a random image</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        </div>
+  )
